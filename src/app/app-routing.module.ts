77cdca405ea29c4df2371ae50d4943ce45aac392
Ricky Goacher <rickyleegoacher@hotmail.co.uk>
2019-04-19 19:09:27 +0100

    Routes added + initial styles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,35 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ProjectComponent } from './project/project.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+	{ path: '', component: HomeComponent },
+	{ path: 'about', component: AboutComponent },
+	{ path: 'contact', component: ContactComponent },
+	{ path: 'dashboard', component: DashboardComponent },
+	{ path: 'login', component: LoginComponent },
+	{ path: 'portfolio', component: PortfolioComponent },
+	{ path: 'portfolio/:id', component: ProjectComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+export const routingComponents = [
+PortfolioComponent, 
+AboutComponent, 
+HomeComponent,
+ContactComponent,
+DashboardComponent,
+LoginComponent,
+ProjectComponent
+];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { HeroComponent } from './hero/hero.component';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProjectComponent } from './project/project.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    HomeComponent,
+    FooterComponent,
+    AboutComponent,
+    ContactComponent,
+    PortfolioComponent,
+    HeroComponent,
+    LoginComponent,
+    DashboardComponent,
+    ProjectComponent
   ],
   imports: [
     BrowserModule,
